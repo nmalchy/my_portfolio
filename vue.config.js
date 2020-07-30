ac355fd6bd6229543bb640b33fd4fc85cfce9b10
Noah Malchy <noahmalchy@gmail.com>
2020-07-29 18:35:31 -0700

    Initial Commit 7/29/2020

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   devServer: {
     disableHostCheck: true,
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/my_portfolio/'
+    : '/',
   },
 
   transpileDependencies: ['vuetify'],
